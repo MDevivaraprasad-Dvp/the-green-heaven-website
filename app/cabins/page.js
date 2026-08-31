94de3fe9f9a389d7a1dbb7fbb97402c273611dbd
M Devi Vara Prasad <thisisdevilogme@gmail.com>
2026-08-31 16:37:26 +0530

    Project Organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <h1>Cabins Page</h1>;
+import Counter from "@/app/_components/Counter";
+
+export default async function Page() {
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return <h1>The Green Heaven . Welcome to Paradise</h1>;
 }
